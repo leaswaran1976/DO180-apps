1f352bbe750dca001f4d898c2d1ecbb098fead20
Student User <student@workstation.lab.example.com>
2022-11-27 13:41:30 -0500

    Fixed bug in console2

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Another change is implemented!!\n"
-print "More changes for console2!!\n"
+print "Another change is implemented!!\n";
+print "More changes for console2!!\n";
 ?>
